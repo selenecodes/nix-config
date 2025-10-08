2415f1780fac4c08baf3031b14ddd426eb0e12e8
Selene Blok <selene.blok@gmail.com>
2025-10-08 19:28:02 +0200

    fix(git): Update git config regarding SSL keys and add git credential manager for Github auth

--- .config/nix/hosts/darwin/shared/home/software/git.nix
+++ .config/nix/hosts/darwin/shared/home/software/git.nix
@@ -8,6 +8,19 @@
     extraConfig = {
       push.autosetupremote = true;
       init.defaultbranch="main";
+      http."https://gitlab.at.rws.nl" = {
+        sslKey  = "~/certs/gitlab-at-rws-nl-cert/git-rws-nl.key";
+        sslCert = "~/certs/gitlab-at-rws-nl-cert/git-rws-nl.pem";
+      };
+      http."https://git.rws.nl" = {
+        sslKey  = "~/certs/gitlab-at-rws-nl-cert/git-rws-nl.key";
+        sslCert = "~/certs/gitlab-at-rws-nl-cert/git-rws-nl.pem";
+      };
+      credential."https://github.com" = {
+        helper = "manager";
+        credentialStore = "cache";
+        username = "selenecodes";
+      };
     };
     signing = {
       format = "openpgp";
