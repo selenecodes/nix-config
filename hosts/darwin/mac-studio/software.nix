337f1460dbb829f5f77806f4f00000493cf57195
Selene Blok <selene.blok@gmail.com>
2026-03-02 16:09:16 +0100

    feat: Add github cli, netbird client

--- hosts/darwin/mac-studio/software.nix
+++ hosts/darwin/mac-studio/software.nix
@@ -8,17 +8,21 @@
   # for both ordinary packages and casks
   homebrew.brews = lib.mkAfter [
     "asimov"
+    "gh"
     # "opencode"
   ];
 
   homebrew.taps = lib.mkAfter [
     # "sst/tap"
+    "netbirdio/tap"
   ];
 
   homebrew.casks = lib.mkAfter [
+    "claude-code"
     # "lm-studio"
     # Shared apps
     # "mp3tag"
+    "netbirdio/tap/netbird-ui"
     # "audiobook-builder"
     # Mac fixes
     # "daisydisk"
