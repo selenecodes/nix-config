4572067303367b9d6ed93bb2d141601a19b4bb73
Selene Blok <selene.blok@gmail.com>
2026-03-15 21:42:12 +0100

    fix(discord): Install discord manually on macos due to screenshare issues.

--- modules/profiles/personal/linux.nix
+++ modules/profiles/personal/linux.nix
@@ -2,6 +2,7 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     google-chrome
+    discord
     plezy
   ];
 }
--- modules/profiles/personal/platform-agnostic.nix
+++ modules/profiles/personal/platform-agnostic.nix
@@ -3,6 +3,5 @@
   environment.systemPackages = with pkgs; [
     claude-code
     netbird
-    discord
   ];
 }
