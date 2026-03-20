0031c4ff7efc1e490c0dcadb5e09828e31b31dd8
Selene Blok <selene.blok@gmail.com>
2026-03-21 00:27:55 +0100

    fix(niri): Update tuigreet niri start and remove unused window block

--- hosts/nixos/gayming/wayland.nix
+++ hosts/nixos/gayming/wayland.nix
@@ -8,7 +8,7 @@
   services.greetd = {
     enable = true;
     settings.default_session.command =
-      "${pkgs.tuigreet}/bin/tuigreet --time --cmd 'niri --session'";
+      "${pkgs.tuigreet}/bin/tuigreet --time --cmd 'niri'";
   };
 
   # Wayland / NVIDIA environment variables
