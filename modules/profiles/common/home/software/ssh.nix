bc3359e0c63db722670c923c72c0c6d10460190a
Selene Blok <selene.blok@gmail.com>
2026-06-24 16:57:34 +0200

    fix: Update ssh config to resolve home-manager deprecation warning

--- modules/profiles/common/home/software/ssh.nix
+++ modules/profiles/common/home/software/ssh.nix
@@ -6,9 +6,8 @@
     enableDefaultConfig = false;
     settings = lib.mkMerge [
       (lib.mkIf pkgs.stdenv.isDarwin {
-        "1password" = {
+        "Match host * exec \"test -z $SSH_CONNECTION\"" = {
           identityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
-          match = "host * exec \"test -z $SSH_CONNECTION\"";
         };
         "rwslaptop" = {
           hostname = "Bloks-MacBook-Air.local";
@@ -17,9 +16,8 @@
         };
       })
       (lib.mkIf (!pkgs.stdenv.isDarwin) {
-        "*" = {
+        "Match host * exec \"test -z $SSH_CONNECTION\"" = {
           identityAgent = "~/.1password/agent.sock";
-          match = "host * exec \"test -z $SSH_CONNECTION\"";
         };
       })
     ];
