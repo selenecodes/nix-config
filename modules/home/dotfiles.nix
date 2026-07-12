1e96266643b96ba5ba285ce6f6e795b5d3da478b
Selene Blok <selene.blok@gmail.com>
2026-07-12 16:46:42 +0200

    chore: Delete unused files

--- modules/home/dotfiles.nix
+++ modules/home/dotfiles.nix
@@ -1,5 +1,4 @@
 { lib, isWork ? false, ... }: {
-  home.file.".config/opencode/opencode.json".source = ./files/opencode-config.json;
   home.file.".condarc".source = ./files/.condarc;
   home.file.".p10k.zsh".source = ./files/.p10k.zsh;
 
