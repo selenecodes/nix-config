42421f2ecf9a0983da97a46dcee3e81328817c85
Selene Blok <selene.blok@gmail.com>
2026-07-25 19:24:16 +0200

    fix(opencode): Use the litellm plugin to make it automatically grab model info

--- modules/packages/opencode.nix
+++ modules/packages/opencode.nix
@@ -5,23 +5,15 @@ _: {
       enable = true;
       package = pkgs.opencode;
       settings = {
+        plugin = ["opencode-plugin-litellm@latest"];
         enabled_providers = ["litellm"];
         model = "litellm/gpt-5.6-luna";
         provider.litellm = {
           npm = "@ai-sdk/openai-compatible";
-          name = "LiteLLM";
+          name = "LiteLLM (proxy)";
           options = {
             baseURL = "http://127.0.0.1:4000/v1";
             apiKey = "{env:LITELLM_API_KEY}";
-          };
-          models = {
-            "gpt-5.1".name = "GPT-5.1";
-            "gpt-5.4".name = "GPT-5.4";
-            "gpt-5.5".name = "GPT-5.5";
-            "gpt-5.6-luna".name = "GPT-5.6 Luna";
-            "gpt-5.6-sol".name = "GPT-5.6 Sol";
-            "gpt-5.6-terra".name = "GPT-5.6 Terra";
-            "text-embedding-3-large".name = "Text Embedding 3 Large";
           };
         };
       };
