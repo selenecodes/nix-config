3aab777dafa607481fa31cba640c57483593f16f
Selene Blok <selene.blok@gmail.com>
2026-08-22 23:49:58 +0200

    fix(nvidia): Upgrade drivers and disable power limit to hopefully fix xid109 errors

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -23,7 +23,14 @@ _: {
       # Open kernel modules are mandatory for Blackwell
       open = true;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "610.43.02";
+        sha256_64bit = "sha256:0qvllxnb20arjhw3bxdz0hw521di9ib75hldzx97gpscpdaa0d1h";
+        sha256_aarch64 = "sha256:0qvllxnb20arjhw3bxdz0hw521di9ib75hldzx97gpscpdaa0d1h";
+        openSha256 = "sha256-hP5NVZZ4vGsACHLmUDKq4uckpd/kn1GxCSYnnJfAuBs=";
+        settingsSha256 = "sha256-0YAhufRgjDW+uR+kjaTb154fibpcDw8QowfrucoZsKE=";
+        persistencedSha256 = "sha256:0nd0bf2s9b2ic8a0rcscddasddkryx2qf6mx4861bv44wblm513z";
+      };
     };
 
     hardware.nvidia-container-toolkit.enable = true;
@@ -38,19 +45,21 @@ _: {
     environment.sessionVariables = {
       GBM_BACKEND = "nvidia-drm";
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      # __GL_SHADER_DISK_CACHE_SKIP_CLEANUP = 1;
       LIBVA_DRIVER_NAME = "nvidia";
     };
 
     # 75% power limit (RTX 5090 TDP = 575W → 431W)
-    systemd.services.nvidia-power-limit = {
-      description = "NVIDIA GPU 75% power limit";
-      wantedBy = ["multi-user.target"];
-      after = ["nvidia-persistenced.service"];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = "${config.hardware.nvidia.package.bin}/bin/nvidia-smi -pl 431";
-      };
-    };
+    # systemd.services.nvidia-power-limit = {
+    #   enable = false;
+    #   description = "NVIDIA GPU 75% power limit";
+    #   wantedBy = ["multi-user.target"];
+    #   after = ["nvidia-persistenced.service"];
+    #   serviceConfig = {
+    #     Type = "oneshot";
+    #     RemainAfterExit = true;
+    #     ExecStart = "${config.hardware.nvidia.package.bin}/bin/nvidia-smi -pl 431";
+    #   };
+    # };
   };
 }
