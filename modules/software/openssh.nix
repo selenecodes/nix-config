055aa521022b9bf111bd24fd235a23b3294c8d66
Selene Blok <selene.blok@gmail.com>
2026-09-02 15:38:31 +0200

    refactor: Target openssh feature

--- modules/software/openssh.nix
+++ modules/software/openssh.nix
@@ -1,44 +1,54 @@
 _: {
-  nixos.networking = {config, ...}: {
-    services.openssh = {
-      enable = true;
-      settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-        PermitRootLogin = "no";
-        X11Forwarding = false;
-        AllowAgentForwarding = true;
-        AllowUsers = [config.myConfig.user.name];
+  repository.features = [
+    {
+      nixos = {
+        targets = ["*"];
+        module = {config, ...}: {
+          services.openssh = {
+            enable = true;
+            settings = {
+              PasswordAuthentication = false;
+              KbdInteractiveAuthentication = false;
+              PermitRootLogin = "no";
+              X11Forwarding = false;
+              AllowAgentForwarding = true;
+              AllowUsers = [config.myConfig.user.name];
+            };
+          };
+        };
       };
-    };
-  };
-  homeManager.base = {
-    pkgs,
-    lib,
-    config,
-    ...
-  }: {
-    options.myConfig.ssh.identityAgent = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = null;
-    };
+      homeManager = {
+        targets = ["*"];
+        module = {
+          pkgs,
+          lib,
+          config,
+          ...
+        }: {
+          options.myConfig.ssh.identityAgent = lib.mkOption {
+            type = lib.types.nullOr lib.types.str;
+            default = null;
+          };
 
-    config.programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-      settings = lib.mkMerge [
-        (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
-          "gayming" = {
-            hostname = "10.10.50.10";
-            forwardAgent = true;
+          config.programs.ssh = {
+            enable = true;
+            enableDefaultConfig = false;
+            settings = lib.mkMerge [
+              (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
+                "gayming" = {
+                  hostname = "10.10.50.10";
+                  forwardAgent = true;
+                };
+              })
+              (lib.mkIf (config.myConfig.ssh.identityAgent != null) {
+                "Match host * exec \"test -z $SSH_CONNECTION\"" = {
+                  identityAgent = config.myConfig.ssh.identityAgent;
+                };
+              })
+            ];
           };
-        })
-        (lib.mkIf (config.myConfig.ssh.identityAgent != null) {
-          "Match host * exec \"test -z $SSH_CONNECTION\"" = {
-            identityAgent = config.myConfig.ssh.identityAgent;
-          };
-        })
-      ];
-    };
-  };
+        };
+      };
+    }
+  ];
 }
