7c27ba3169392cef004ec58e23ec188335a96fde
Selene Blok <selene.blok@gmail.com>
2026-09-02 13:08:08 +0200

    refactor: Target Ananicy

--- modules/software/ananicy.nix
+++ modules/software/ananicy.nix
@@ -1,9 +1,16 @@
 _: {
-  nixos.gaming = {pkgs, ...}: {
-    services.ananicy = {
-      enable = true;
-      package = pkgs.ananicy-cpp;
-      rulesProvider = pkgs.ananicy-rules-cachyos;
-    };
-  };
+  repository.features = [
+    {
+      nixos = {
+        targets = ["gayming"];
+        module = {pkgs, ...}: {
+          services.ananicy = {
+            enable = true;
+            package = pkgs.ananicy-cpp;
+            rulesProvider = pkgs.ananicy-rules-cachyos;
+          };
+        };
+      };
+    }
+  ];
 }
