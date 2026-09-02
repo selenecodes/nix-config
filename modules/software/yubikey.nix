90db595d0d41179ce8939f24f3144b1e94eb3b59
Selene Blok <selene.blok@gmail.com>
2026-09-02 13:08:27 +0200

    refactor: Target YubiKey

--- modules/software/yubikey.nix
+++ modules/software/yubikey.nix
@@ -1,11 +1,18 @@
 _: {
-  nixos.base = {pkgs, ...}: {
-    services.udev.packages = [pkgs.yubikey-personalization];
+  repository.features = [
+    {
+      nixos = {
+        targets = ["gayming" "rwslaptop"];
+        module = {pkgs, ...}: {
+          services.udev.packages = [pkgs.yubikey-personalization];
 
-    environment.systemPackages = with pkgs; [
-      yubikey-manager
-      yubioath-flutter
-      yubikey-touch-detector
-    ];
-  };
+          environment.systemPackages = with pkgs; [
+            yubikey-manager
+            yubioath-flutter
+            yubikey-touch-detector
+          ];
+        };
+      };
+    }
+  ];
 }
