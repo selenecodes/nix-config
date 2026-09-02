17616204ae16f18594c55d4bd1155c67d4e5c193
Selene Blok <selene.blok@gmail.com>
2026-09-02 13:08:41 +0200

    refactor: Target NVIDIA

--- modules/software/nvidia.nix
+++ modules/software/nvidia.nix
@@ -1,31 +1,38 @@
 _: {
-  nixos.nvidia = _: {
-    nix.settings = {
-      substituters = ["https://cache.nixos-cuda.org"];
-      trusted-public-keys = ["cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="];
-    };
+  repository.features = [
+    {
+      nixos = {
+        targets = ["gayming" "rwslaptop"];
+        module = {
+          nix.settings = {
+            substituters = ["https://cache.nixos-cuda.org"];
+            trusted-public-keys = ["cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="];
+          };
 
-    boot = {
-      kernelParams = ["nvidia_drm.modeset=1" "nvidia_drm.fbdev=1"];
-      initrd.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
-    };
+          boot = {
+            kernelParams = ["nvidia_drm.modeset=1" "nvidia_drm.fbdev=1"];
+            initrd.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
+          };
 
-    hardware = {
-      nvidia = {
-        modesetting.enable = true;
-        nvidiaSettings = true;
+          hardware = {
+            nvidia = {
+              modesetting.enable = true;
+              nvidiaSettings = true;
+            };
+            nvidia-container-toolkit.enable = true;
+            graphics.enable = true;
+          };
+
+          services.xserver.videoDrivers = ["nvidia"];
+
+          environment.sessionVariables = {
+            GBM_BACKEND = "nvidia-drm";
+            __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+            # __GL_SHADER_DISK_CACHE_SKIP_CLEANUP = 1;
+            LIBVA_DRIVER_NAME = "nvidia";
+          };
+        };
       };
-      nvidia-container-toolkit.enable = true;
-      graphics.enable = true;
-    };
-
-    services.xserver.videoDrivers = ["nvidia"];
-
-    environment.sessionVariables = {
-      GBM_BACKEND = "nvidia-drm";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      # __GL_SHADER_DISK_CACHE_SKIP_CLEANUP = 1;
-      LIBVA_DRIVER_NAME = "nvidia";
-    };
-  };
+    }
+  ];
 }
