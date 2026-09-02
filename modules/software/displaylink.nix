22dac0295a24e82f11b33a628f8b8bedc1052d43
Selene Blok <selene.blok@gmail.com>
2026-09-02 13:17:27 +0200

    refactor: Target DisplayLink

--- modules/software/displaylink.nix
+++ modules/software/displaylink.nix
@@ -1,20 +1,28 @@
 _: {
-  nixos.work = {
-    config,
-    lib,
-    pkgs,
-    ...
-  }: {
-    options.myConfig.displaylink.enable = lib.mkEnableOption "DisplayLink";
+  repository.features = [
+    {
+      nixos = {
+        targets = ["rwslaptop"];
+        module = {
+          config,
+          lib,
+          pkgs,
+          ...
+        }: {
+          options.myConfig.displaylink.enable = lib.mkEnableOption "DisplayLink";
 
-    config = lib.mkIf config.myConfig.displaylink.enable {
-      environment.systemPackages = [pkgs.displaylink];
-      services.xserver.videoDrivers = ["displaylink" "modesetting"];
-      systemd.services.dlm.wantedBy = ["multi-user.target"];
-    };
-  };
+          config = lib.mkIf config.myConfig.displaylink.enable {
+            environment.systemPackages = [pkgs.displaylink];
+            services.xserver.videoDrivers = ["displaylink" "modesetting"];
+            systemd.services.dlm.wantedBy = ["multi-user.target"];
+          };
+        };
+      };
 
-  darwin.work = _: {
-    homebrew.casks = ["displaylink"];
-  };
+      darwin = {
+        targets = ["studio"];
+        module.homebrew.casks = ["displaylink"];
+      };
+    }
+  ];
 }
