2845a6734d969d1db9c40f623b9f7e956d787963
Selene Blok <selene.blok@gmail.com>
2026-09-23 09:52:44 +0200

    fix: Enable flakes + nix-command

--- modules/config/base/nixos.nix
+++ modules/config/base/nixos.nix
@@ -4,6 +4,8 @@ _: {
       nixos = {
         targets = ["*"];
         module = {lib, ...}: {
+          config.nix.settings.experimental-features = ["nix-command" "flakes"];
+
           options.myConfig.user.name = lib.mkOption {
             type = lib.types.str;
             description = "Primary user name for this host";
